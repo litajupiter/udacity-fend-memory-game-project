f68d6b2024ea0e530057a9671a2bea3ad12a998a
Brianne <brianne.hennel@gmail.com>
2018-05-29 19:42:42 -0400

    feat: card deck is now programmatically generated

--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
 /*
  * Create a list that holds all of your cards
  */
+const cards = ['fa-diamond', 'fa-diamond',
+               'fa-paper-plane-o', 'fa-paper-plane-o',
+               'fa-anchor', 'fa-anchor',
+               'fa-bolt', 'fa-bolt',
+               'fa-cube', 'fa-cube',
+               'fa-leaf', 'fa-leaf',
+               'fa-bicycle', 'fa-bicycle',
+               'fa-bomb', 'fa-bomb'
+              ];
+
+function generateCard(card) {
+    return `<li class="card"><i class="fa ${card}"></i></li>`;
+}
 
 
 /*
@@ -36,6 +49,19 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+function initGame() {
+    let deck = document.querySelector('.deck');
+    let cardHTML = cards.map(function(card) {
+        return generateCard(card);
+    });
+
+    deck.innerHTML = cardHTML.join('');
+}
+
+initGame();
+
+
 const allCards = document.querySelectorAll('.card');
 let openCards = [];
 
@@ -46,6 +72,12 @@ allCards.forEach(function(card) {
           openCards.push(card);
           card.classList.add('open', 'show');
 
+
+          //Check if the cards match
+          let firstCardType = openCards[0].dataset.card;
+          console.log(firstCardType);
+
+          //If cards don't match - go away
           if (openCards.length == 2) {
               setTimeout(function() {
                 openCards.forEach(function(card) {
