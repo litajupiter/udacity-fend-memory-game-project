201446a0e230630b758d3014bdda8be930d3060e
Brianne <brianne.hennel@gmail.com>
2018-06-11 16:34:48 -0400

    feat: added starter code for winning modal

--- js/app.js
+++ js/app.js
@@ -122,3 +122,30 @@ allCards.forEach(function(card) {
   });
 });
 //if all cards have matched, display a message with the final score
+
+//code below from w3schools how to make a modal box -- needs edited
+//Get the modal
+var modal = document.getElementById('winModal');
+
+//Get the button that opens the modal
+var btn = document.getElementById('myBtn');
+
+//Get the <span> element that closes the modal
+var span = document.getElementsByClassName('close')[0];
+
+//When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+//When the user clicks on <span>(x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+//When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
